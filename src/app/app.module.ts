586ae20cf8b155b76a0d575a7cc71325ab947ad6
Levan Beroshvili <lberoshvili9@gmail.com>
2020-05-03 21:27:47 +0400

    Moved shell header and shell sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthComponent } from './auth/auth.component';
 import { ShellComponent } from './shell/shell.component';
+import { ShellHeaderComponent } from './shell/shell-header/shell-header.component';
+import { ShellSidebarComponent } from './shell/shell-sidebar/shell-sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
-    ShellComponent
+    ShellComponent,
+    ShellHeaderComponent,
+    ShellSidebarComponent
   ],
   imports: [
     BrowserModule,
